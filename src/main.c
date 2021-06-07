df9e78722b0c16eb6c74925cf105383160e9f8bf
DBozhkovV <danail.g.bozhkov.2018@elsys-bg.com>
2021-06-07 18:00:22 +0300

    Add main menu code

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 // ============================================================================================= //
 /* Structures, Global variables, Function declarations */
 
+int startingMenu();
 
 
 // ============================================================================================= //
@@ -20,6 +21,41 @@ int main() {
 // ============================================================================================= //
 /* Function definitoins */
 
-
+int startingMenu(){
+	int menu;
+	system("clear");
+	
+	do{
+	printf("	(1)	New Game  \n");
+	printf("	(2)	Settings  \n");
+	printf("	(3)	Enter word in dictionary  \n");
+	printf("	(4)	Exit  \n");
+	printf("--------------------------------------------- \n");
+	scanf("%d", &menu);
+	
+	switch(menu)
+	{
+		case 1: printf("zapochva igrata"); // Tyk trqbva da se dobavi funkciqta za IGRATA
+			break;
+			
+		case 2: printf("otivame v settings"); // Tyk trqbva fynkicq za settings 
+			break;
+			
+		case 3: printf("trqbva da vuvedem dyma v rechnika"); // Tyk trqbva fynkciq za dobavqne na dyma v rechnika
+			break;
+			
+		case 4: printf("izlizame ot igrata");
+			return 0;
+			break;
+			
+		default: system("clear");
+			printf("Nevalidno, probvai pak\n");
+			break;
+	}
+	
+	}while (menu < 0 || menu > 5);
+	
+	system("clear");
+}
 
 // ============================================================================================= //
