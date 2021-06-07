56068d1ee2989f99d53e383856eac070dcc1aa1c
YassenEfremov <yassen.efremov@gmail.com>
2021-06-07 21:17:23 +0300

    Add game logic and settings header files

---
+++ src/game_settings/game_settings.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+extern void gameSettings(int *letters, int *rounds) {
+
+    // Here are the sittings for letters and rounds
+
+    printf("Set the number of letters and rounds\n");
+
+}
--- src/main.c
+++ src/main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "./game_logic/game_logic.h"
+#include "./game_settings/game_settings.h"
 
 
 // ============================================================================================= //
 /* Structures, Global variables, Function declarations */
 
-int startingMenu();
+
+void startingMenu();
 
 
 // ============================================================================================= //
@@ -12,7 +18,7 @@ int startingMenu();
 
 int main() {
     
-    
+    startingMenu();
 
     return 0;
 }
@@ -21,41 +27,55 @@ int main() {
 // ============================================================================================= //
 /* Function definitoins */
 
-int startingMenu(){
+
+void startingMenu() {
+
+    system("clear");
+
 	int menu;
-	system("clear");
+	int letters = 10; // default
+    int rounds = 10; // default
+
+	do {
+        printf("--------------------------------------------- \n");
+	    printf("	(1)	New Game  \n");
+	    printf("	(2)	Settings  \n");
+	    printf("	(3)	Enter word in dictionary  \n");
+	    printf("	(4)	Exit  \n");
+	    printf("--------------------------------------------- \n");
+	    scanf("%d", &menu);
 	
-	do{
-	printf("	(1)	New Game  \n");
-	printf("	(2)	Settings  \n");
-	printf("	(3)	Enter word in dictionary  \n");
-	printf("	(4)	Exit  \n");
-	printf("--------------------------------------------- \n");
-	scanf("%d", &menu);
+	    switch(menu) {
+
+	    	case 1:
+                //printf("zapochva igrata");
+                startGame(letters, rounds); // funkciqta za IGRATA
+	    		break;
+			
+	    	case 2:
+                //printf("otivame v settings");
+                gameSettings(&letters, &rounds); // fynkicq za settings
+	    		break;
+			
+	    	case 3:
+                printf("trqbva da vuvedem dyma v rechnika"); // Tyk trqbva fynkciq za dobavqne na dyma v rechnika
+	    		break;
+			
+	    	case 4:
+                system("clear"); 
+                exit(EXIT_SUCCESS); // izlizame ot igrata
+	    		//break;
+			
+	    	default:
+                system("clear");
+	    		printf("Nevalidno, probvai pak\n");
+	    		break;
+	    }
 	
-	switch(menu)
-	{
-		case 1: printf("zapochva igrata"); // Tyk trqbva da se dobavi funkciqta za IGRATA
-			break;
-			
-		case 2: printf("otivame v settings"); // Tyk trqbva fynkicq za settings 
-			break;
-			
-		case 3: printf("trqbva da vuvedem dyma v rechnika"); // Tyk trqbva fynkciq za dobavqne na dyma v rechnika
-			break;
-			
-		case 4: printf("izlizame ot igrata");
-			return 0;
-			break;
-			
-		default: system("clear");
-			printf("Nevalidno, probvai pak\n");
-			break;
-	}
-	
-	}while (menu < 0 || menu > 5);
+	} while (menu < 0 || menu > 5);
 	
 	system("clear");
 }
+
 
 // ============================================================================================= //
