84bf246ce593c8047a2773a64a49c6d1e9ad727a
DBozhkovV <danail.g.bozhkov.2018@elsys-bg.com>
2021-06-07 22:03:08 +0300

    Add basic game settings

--- src/game_settings/game_settings.h
+++ src/game_settings/game_settings.h
@@ -1,9 +1,43 @@
 #include <stdio.h>
 
+// Here are the sittings for letters and rounds
+
 extern void gameSettings(int *letters, int *rounds) {
-
-    // Here are the sittings for letters and rounds
-
-    printf("Set the number of letters and rounds\n");
-
+	int choice;	
+	int num_letters2;
+	int num_rounds2;
+	
+	system("clear");
+	
+	do{
+	
+	printf(" 	(1)   Promenqne na broq na podavanite bykvi \n");
+	printf(" 	(2)	  Promenqne na broq ryndove \n");
+	scanf("%d", &choice);
+	
+	if(choice == 1){
+		printf("Kolko bykwi jelaete: \n");
+		scanf("%d", &num_letters2);
+		
+		if(num_letters2 < 0 && num_letters2 > 26){
+			printf("nevalidno");
+			return;
+		}
+		*letters = num_letters2;
+		
+	}else if(choice == 2){
+		printf("Kolko runda jelaete:  \n");
+		scanf("%d", &num_rounds2);
+		
+		if(num_rounds2 < 0 && num_rounds2 > 100){
+			printf("Nevalidno");
+			return;	
+		}
+			*rounds = num_rounds2;	
+		}
+		
+	}while (choice < 0 || choice > 3);
 }
+
+
+
