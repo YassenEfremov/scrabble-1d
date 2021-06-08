b4a3442adb65a4ca4c8e9fe8cc01f571d70a8382
DBozhkovV <73282224+DBozhkovV@users.noreply.github.com>
2021-06-08 19:14:00 +0300

    Add random letter generation

--- src/game_logic/game_logic.h
+++ src/game_logic/game_logic.h
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//  Funkciq za generirane na bykwi za edin round i printiraneto im
+extern void letter_generation(int letters){
+    int random_letter;
+    int array[letters + 1];
+    
+    srand(time(0)); // generira mi random chislo, ot koeto zavisqt random chislata
+    
+    for(int i=0; i < letters; i++){
+        // izpolzvam formula za generiraneto na slychaina bykwa -> (rand() % (upper - lower + 1)) + lower;
+        random_letter = (rand() % (122 - 97 + 1)) + 97; 
+        array[i] = random_letter;
+        printf("%c  | ", array[i]);
+    }
+}
 
 extern void startGame(int letters, int rounds) {
-
-    // Here goes the game logic
-
-    printf("The game begins!\n");
-
+    letter_generation(letters);
+    
     /*
-
     1. Във всеки рунд се генерират няколко на брой букви и се изписват на екрана.
     2. Потребителят въвежда дума, която се състои само от предоставените букви.
         - При въвеждане на невалидна дума (която съдържа други букви или повече от подадения брой от дадена буква) се
