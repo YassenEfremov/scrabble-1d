9918ed1bb53eb70bd88b618f1c04b72e1950a60b
Lazo <lazo.velinov@gmail.com>
2021-06-09 23:42:02 +0300

    fix game logic warnings

--- src/game_logic/game_logic.h
+++ src/game_logic/game_logic.h
@@ -2,10 +2,52 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+
+extern int enter_and_check(char rand_letters[], int letters){
+
+	char word[letters];
+	// int flag = 0;
+	
+	printf("Enter word to play or enter 9 to skip level:  ");
+	scanf("%s", word);
+	
+	
+	char quit_word[] = "9";
+	
+	if(strcmp(word, quit_word) == 0){
+		return -1;
+	}
+
+	for(int i = 0; word[i] != '\0'; i++){
+
+		int flag = 0;
+	
+		for(int j = 0; rand_letters[j] != '\0'; j++){
+
+			if(rand_letters[j] == word[i]){
+				flag = 1;
+				rand_letters[j] = '-';
+				break;
+			}
+
+		}
+
+		if(flag == 0){
+			return 0;
+		}
+
+	}
+
+	return 1;
+}
+
+
+
 //  Funkciq za generirane na bykwi za edin round i printiraneto im
 extern void letter_generation(int letters){
     int random_letter;
-    int array[letters + 1];
+    char array[letters + 1];
     
     srand(time(0)); // generira mi random chislo, ot koeto zavisqt random chislata
     
@@ -13,35 +55,28 @@ extern void letter_generation(int letters){
         // izpolzvam formula za generiraneto na slychaina bykwa -> (rand() % (upper - lower + 1)) + lower;
         random_letter = (rand() % (122 - 97 + 1)) + 97; 
         array[i] = random_letter;
-        printf("%c  | ", array[i]);
+        printf("%c", array[i]);
     }
-}
+	array[letters] = '\0';
 
-extern void enter_and_check(char ran_letters[]){
 
-	char *word;
-	int flag = 0;
-	
-	printf("Enter word:  ");
-	scanf("%s", &word);
-	
-	
-		for(int i=0;  !=  ; i++){
-		printf("2\n");
-			for(int j=0; j < b - 1; j++){
-			printf("3\n");
-				if(word[i] == ran_letters[j]){
-					ran_letters[j] = ''; // za da mahna slychai v koito dymata na potrebitelq 									  izpolzva edna vykwa dva puti
-					flag = 1;
-					printf("4\n");
-			}
-				printf("1234\n");
-		}
-	}
+	int flag;
+
+	do{
+
+	flag = enter_and_check(array, letters);
+
+	}while(flag == 0);
+
 }
 
 
-extern void startGame(int letters, int rounds) {
+
+extern void startGame(int letters, int rounds){
+
+	
     letter_generation(letters);
     //enter_and_check(letters);
+
+	return;
 }
