c66f4b772b65e2bb485eb4846a7fa84a5a2fbbf1
DBozhkovV <73282224+DBozhkovV@users.noreply.github.com>
2021-06-10 18:11:06 +0300

    Fix user input validation

--- src/game_settings/game_settings.h
+++ src/game_settings/game_settings.h
@@ -3,41 +3,56 @@
 // Here are the sittings for letters and rounds
 
 extern void gameSettings(int *letters, int *rounds) {
-	int choice;	
-	int num_letters2;
-	int num_rounds2;
+    int choice;    
+    int num_letters2;
+    int num_rounds2;
+    
+    system("clear");
+    
+    do{
+    
+    printf("     (1)   Change number of letters: \n");
+    printf("     (2)      Change number of rounds: \n");
+    scanf("%d", &choice);
+    
+    if(choice == 1){
+    
+        int flag = 0;
+        
+        do{
+            printf("How many letters do you want: \n");
+            scanf("%d", &num_letters2);
+        
+            if(num_letters2 < 3 || num_letters2 > 27){
+                printf("\nInvalid, try again !\n");
+                continue;
+            }
+            
+            flag = 1;
+            *letters = num_letters2;
+            
+        }while(flag != 1);
+        
+    }else if(choice == 2){
+    
+        int flag = 0;
+        
+        do{
+            printf("How many rounds do you want:  \n");
+            scanf("%d", &num_rounds2);    
+                
+            if(num_rounds2 < 1 || num_rounds2 > 100){
+                printf("\nInvalid, try again ! \n");
+                continue;    
+            }
+            
+            flag = 1;
+            *rounds = num_rounds2;
+                
+        }while(flag != 1);
+    
+    }
+            
+    }while (choice < 0 || choice > 3);
 	
-	system("clear");
-	
-	do{
-	
-	printf(" 	(1)   Promenqne na broq na podavanite bykvi \n");
-	printf(" 	(2)	  Promenqne na broq ryndove \n");
-	scanf("%d", &choice);
-	
-	if(choice == 1){
-		printf("Kolko bykwi jelaete: \n");
-		scanf("%d", &num_letters2);
-		
-		if(num_letters2 < 0 || num_letters2 > 27){
-			printf("nevalidno");
-			return;
-		}
-		*letters = num_letters2;
-		
-	}else if(choice == 2){
-		printf("Kolko runda jelaete:  \n");
-		scanf("%d", &num_rounds2);
-		
-		if(num_rounds2 < 0 || num_rounds2 > 100){
-			printf("Nevalidno");
-			return;	
-		}
-			*rounds = num_rounds2;	
-		}
-		
-	}while (choice < 0 || choice > 3);
 }
-
-
-
